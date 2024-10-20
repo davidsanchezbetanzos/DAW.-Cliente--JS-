be063286ea511fac2857cdd2e699991d149a3138
David Sanchez <davidsgomez@ext.inditex.com>
2024-10-20 18:45:38 +0200

    1er commit. Probando conexion vscode-github

---
+++ tarea2.js
@@ -0,0 +1,6 @@
+/*Realizar una pequeña aplicación en JavaScript que muestre lo siguiente POR CONSOLA:
+
+Tabla de sumar de un número que se ha de pedir al usuario por teclado usando prompt. Se comprobará que el número es correcto, en caso de que no sea correcto, se mostrará un mensaje por consola. Utilizar plantillas literales para la presentación. El cero se considerará un número válido para este caso.
+Script que muestre las tabla de multiplicar de los números del 1 al 10, una a continuación de la otra. Utilizar plantillas literales para la presentación. Ten en cuenta que primero irá un enunciado "Tablas de multiplicar" y, luego cada tabla irá precedida de un título que indique qué tabla se muestra (ver imagen). En este apartado se han de usar dos tipos de bucles diferentes, aunque no sea lo más apropiado.*/
+
+alert ("Hola mundo");
