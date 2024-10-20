f0c4a4a46be2e89e7de2f5318a93fd54e1ef65ff
David Sanchez <davidsgomez@ext.inditex.com>
2024-10-20 18:48:34 +0200

    2º commit test

--- tarea2.js
+++ tarea2.js
@@ -4,3 +4,4 @@ Tabla de sumar de un número que se ha de pedir al usuario por teclado usando pr
 Script que muestre las tabla de multiplicar de los números del 1 al 10, una a continuación de la otra. Utilizar plantillas literales para la presentación. Ten en cuenta que primero irá un enunciado "Tablas de multiplicar" y, luego cada tabla irá precedida de un título que indique qué tabla se muestra (ver imagen). En este apartado se han de usar dos tipos de bucles diferentes, aunque no sea lo más apropiado.*/
 
 alert ("Hola mundo");
+alert ("Hola mundo 2º COMMIT");
